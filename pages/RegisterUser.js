53dd0542c7764fd7428bb4c9c52e2bd0d6dbad5d
Viktor Izotov <vittozich@gmail.com>
2020-07-07 19:16:41 +0300

    init working version react (old: classes) with sqlite

---
+++ pages/RegisterUser.js
@@ -0,0 +1,100 @@
+/*Screen to register the user*/
+import React from 'react';
+import {View, ScrollView, KeyboardAvoidingView, Alert} from 'react-native';
+import Mytextinput from './components/Mytextinput';
+import Mybutton from './components/Mybutton';
+import {openDatabase} from 'react-native-sqlite-storage';
+
+var db = openDatabase({name: 'UserDatabase.db'});
+export default class RegisterUser extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user_name: '',
+      user_contact: '',
+      user_address: '',
+    };
+  }
+
+  register_user = () => {
+    var that = this;
+    const {user_name} = this.state;
+    const {user_contact} = this.state;
+    const {user_address} = this.state;
+    //alert(user_name, user_contact, user_address);
+    if (user_name) {
+      if (user_contact) {
+        if (user_address) {
+          db.transaction(function(tx) {
+            tx.executeSql(
+              'INSERT INTO table_user (user_name, user_contact, user_address) VALUES (?,?,?)',
+              [user_name, user_contact, user_address],
+              (tx, results) => {
+                console.log('Results', results.rowsAffected);
+                if (results.rowsAffected > 0) {
+                  Alert.alert(
+                    'Success',
+                    'You are Registered Successfully',
+                    [
+                      {
+                        text: 'Ok',
+                        onPress: () =>
+                          that.props.navigation.navigate('HomeScreen'),
+                      },
+                    ],
+                    {cancelable: false},
+                  );
+                } else {
+                  alert('Registration Failed');
+                }
+              },
+            );
+          });
+        } else {
+          alert('Please fill Address');
+        }
+      } else {
+        alert('Please fill Contact Number');
+      }
+    } else {
+      alert('Please fill Name');
+    }
+  };
+
+  render() {
+    return (
+      <View style={{backgroundColor: 'white', flex: 1}}>
+        <ScrollView keyboardShouldPersistTaps="handled">
+          <KeyboardAvoidingView
+            behavior="padding"
+            style={{flex: 1, justifyContent: 'space-between'}}>
+            <Mytextinput
+              placeholder="Enter Name"
+              onChangeText={user_name => this.setState({user_name})}
+              style={{padding: 10}}
+            />
+            <Mytextinput
+              placeholder="Enter Contact No"
+              onChangeText={user_contact => this.setState({user_contact})}
+              maxLength={10}
+              keyboardType="numeric"
+              style={{padding: 10}}
+            />
+            <Mytextinput
+              placeholder="Enter Address"
+              onChangeText={user_address => this.setState({user_address})}
+              maxLength={225}
+              numberOfLines={5}
+              multiline={true}
+              style={{textAlignVertical: 'top', padding: 10}}
+            />
+            <Mybutton
+              title="Submit"
+              customClick={this.register_user.bind(this)}
+            />
+          </KeyboardAvoidingView>
+        </ScrollView>
+      </View>
+    );
+  }
+}
